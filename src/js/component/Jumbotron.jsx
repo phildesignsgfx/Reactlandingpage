e8413e53e7718f45cc662f7aadc675d0a66f1483
Philippe <82619578+phildesignsgfx@users.noreply.github.com>
2023-10-14 21:32:00 +0000

    done5

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -5,13 +5,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Jumbotron = () => {
-	return ( <div class="jumbotron m-5 bg-light-subtle">
-    <h1 class="display-4 bold">A Warm Welcome</h1>
-    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	return ( <div className="jumbotron m-5 bg-secondary-subtle">
+    <h1 className="display-4 bold">A Warm Welcome</h1>
+    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
    
     <p></p>
-    <p class="lead">
-      <a class="btn btn-primary btn-lg" href="#" role="button">Call to Action</a>
+    <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Call to Action</a>
     </p>
   </div>
 
--- src/js/component/smallcards.jsx
+++ src/js/component/smallcards.jsx
@@ -5,13 +5,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const SmallCards = () => {
-	return (<div class="row row-cols-1 row-cols-md-3 g-4">
+	return (<div class="row row-cols-1 row-cols-md-4 g-4 m-4">
     <div class="col">
       <div class="card">
         <img src="https://picsum.photos/500/325?random=1" class="card-img-top" alt="..."/>
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Find out more</a>
+    </p>
         </div>
       </div>
     </div>
@@ -21,6 +24,9 @@ const SmallCards = () => {
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Find out more</a>
+    </p>
         </div>
       </div>
     </div>
@@ -30,6 +36,9 @@ const SmallCards = () => {
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+          <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Find out more</a>
+    </p>
         </div>
       </div>
     </div>
@@ -39,6 +48,9 @@ const SmallCards = () => {
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p className="lead">
+      <a className="btn btn-primary btn-lg" href="#" role="button">Find out more</a>
+    </p>
         </div>
       </div>
     </div>
